57b2c13b7ca1c12b175e30db58e427cf9f8ade77
Alex Coronell <alexcoronell@gmail.com>
2024-11-20 22:57:46 -0500

    SalesService created and completed - NOT TESTED

--- src/app/core/services/sales.service.ts
+++ src/app/core/services/sales.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+
+import { CreateSaleDto, UpdateSaleDto } from '../dtos/Sale.dto';
+import { Sale } from '../models/Sale.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SalesService {
+  private url = environment.apiURL + '/sales';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getAll = async (page = 1, limit = 10) =>
+    await this.http.get(`${this.url}?page=${page}&limit=${limit}`);
+
+  get = async (id: Sale['id']) => await this.http.get(`${this.url}/${id}`);
+
+  create = async (dto: CreateSaleDto) => await this.http.post(this.url, dto);
+
+  update = async (id: Sale['id'], dto: UpdateSaleDto) =>
+    this.http.put(`${this.url}/${id}`, dto);
+
+  delete = async (id: Sale['id']) => this.http.delete(`${this.url}/${id}`);
 }
