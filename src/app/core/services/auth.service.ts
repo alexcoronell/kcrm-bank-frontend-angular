4608aa3e581ee465fc88dbd5c64570ece1897f3e
Alex Coronell <alexcoronell@gmail.com>
2024-11-20 20:05:36 -0500

    FranchiseService completed - NOT TESTED

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+
+import { LoginDto } from '../dtos/Login.dto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private url = environment.apiURL;
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  login = async (dto: LoginDto) =>
+    await this.http.post(`${this.url}/login`, dto, { withCredentials: true });
+
+  logout = async () =>
+    await this.http.get(`${this.url}/logout`, { withCredentials: true });
+
+  verifySession = async () =>
+    await this.http.get(`${this.url}/verify-sesion`, { withCredentials: true });
 }
--- src/app/core/services/franchises.service.ts
+++ src/app/core/services/franchises.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+
+import { CreateFranchiseDto, UpdateFracchiseDto } from '../dtos/Franchise.dto';
+import { Franchise } from '../models/Franchise.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FranchisesService {
+  private url = environment.apiURL;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getAll = async (page = 1, limit = 10) =>
+    await this.http.get(`${this.url}?page=${page}&limit=${limit}`, {
+      withCredentials: true,
+    });
+
+  get = async (id: Franchise['id']) =>
+    await this.http.get(`${this.url}/${id}`, { withCredentials: true });
+
+  create = async (dto: CreateFranchiseDto) =>
+    await this.http.post(`url`, dto, { withCredentials: true });
+
+  update = async (id: Franchise['id'], dto: UpdateFracchiseDto) =>
+    await this.http.put(`${this.url}/${id}`, dto, { withCredentials: true });
+
+  delete = async (id: Franchise['id']) =>
+    await this.http.delete(`${this.url}/${id}`, { withCredentials: true });
 }
