6409c6e968630d8a14fb1e9e165911fb079da9ff
Alex Coronell <alexcoronell@gmail.com>
2024-11-20 22:59:23 -0500

    UserssService created and completed - NOT TESTED

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
+
+import { CreateUserDto, UpdateUserDto } from '../dtos/User.dto';
+import { User } from '../models/User,interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  private url = environment.apiURL + '/users';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  getAll = async (page = 1, limit = 10) =>
+    await this.http.get(`${this.url}?page=${page}&limit=${limit}`);
+
+  get = async (id: User['id']) => await this.http.get(`${this.url}/${id}`);
+
+  create = async (dto: CreateUserDto) => await this.http.post(this.url, dto);
+
+  update = async (id: User['id'], dto: UpdateUserDto) =>
+    this.http.put(`${this.url}/${id}`, dto);
+
+  delete = async (id: User['id']) => this.http.delete(`${this.url}/${id}`);
 }
