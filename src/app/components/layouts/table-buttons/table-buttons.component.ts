d948abe38a587aae292ede06ffaefaef73ac5ca7
Alex Coronell <alexcoronell@gmail.com>
2024-11-26 20:49:27 -0500

    Table buttons basic layout

---
+++ src/app/components/layouts/table-buttons/table-buttons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../../modules/material/material.module';
+
+@Component({
+  selector: 'app-table-buttons',
+  imports: [MaterialModule],
+  templateUrl: './table-buttons.component.html',
+  styleUrl: './table-buttons.component.scss',
+})
+export class TableButtonsComponent {}
--- src/app/pages/private/franchises-page/franchises-page.component.ts
+++ src/app/pages/private/franchises-page/franchises-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TableLayoutComponent } from '../../../components/layouts/table-layout/table-layout.component';
+import { TableButtonsComponent } from '../../../components/layouts/table-buttons/table-buttons.component';
 import { FranchisesComponent } from '../../../components/tables/franchises/franchises.component';
 
 @Component({
   selector: 'app-franchises-page',
-  imports: [TableLayoutComponent, FranchisesComponent],
+  imports: [TableLayoutComponent, TableButtonsComponent, FranchisesComponent],
   templateUrl: './franchises-page.component.html',
   styleUrl: './franchises-page.component.scss',
 })
