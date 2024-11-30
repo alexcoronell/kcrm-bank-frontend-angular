8d5332bfcd2e3525909e737656125e32c3f9c81a
Alex Coronell <alexcoronell@gmail.com>
2024-11-30 00:27:59 -0500

    Basic layout form Create Franquise

--- src/app/components/forms/franchises-form/franchises-form.component.ts
+++ src/app/components/forms/franchises-form/franchises-form.component.ts
@@ -4,7 +4,20 @@ import {
   signal,
   OnChanges,
   SimpleChanges,
+  inject,
 } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+/* Services */
+import { FranchisesService } from '../../../core/services/franchises.service';
+
+/* Modules */
+import { MaterialModule } from '../../../modules/material/material.module';
 
 /* Componets */
 import { LayoutFormComponent } from '../../layouts/layout-form/layout-form.component';
@@ -16,29 +29,59 @@ import { Franchise } from '../../../core/models/Franchise.interface';
 import { RequestStatus } from '../../../core/types/RequestStatus.type';
 import { StatusMode } from '../../../core/types/StatusMode.type';
 
+/* Helpers */
+import { MyErrorStateMatcher } from '../../../core/helpers/MyErrorStateMatcher.helper';
+
 @Component({
   selector: 'app-franchises-form',
-  imports: [LayoutFormComponent],
+  imports: [LayoutFormComponent, ReactiveFormsModule, MaterialModule],
   templateUrl: './franchises-form.component.html',
   styleUrl: './franchises-form.component.scss',
 })
 export class FranchisesFormComponent implements OnChanges {
+  /****************************************** Services ******************************************/
+  private formBuilder = inject(FormBuilder);
+  private franchisesService = inject(FranchisesService)
+
   @Input() id: Franchise['id'] | null = null;
   @Input() showFormInput = false;
+
+  /****************************************** Signals ******************************************/
   requestStatus = signal<RequestStatus>('init');
   statusMode = signal<StatusMode>('create');
   showForm = signal<boolean>(false);
 
+  /****************************************** Properties ******************************************/
+  title = 'Crear Franquicia';
+  form: FormGroup;
+  errorNameFormControl = new MyErrorStateMatcher();
+
+  /****************************************** Contructor ******************************************/
+  constructor() {
+    this.buildForm();
+  }
+
+  /****************************************** OnChanges ******************************************/
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['id'] && changes['id'].currentValue) {
       this.fetchData(changes['id'].currentValue);
+      this.title = "Detalle de Franquicia"
     }
     if (changes['showFormInput'] && changes['showFormInput'].currentValue) {
-      console.log(changes['showFormInput'].currentValue)
+      console.log(changes['showFormInput'].currentValue);
       this.showForm.set(changes['showFormInput'].currentValue);
     }
   }
 
+  /****************************************** Methods ******************************************/
+   /****** Build Form ******/
+   private buildForm(){
+    this.form = this.formBuilder.group({
+      nameFormControl :['', [Validators.required]]
+    })
+   }
+
+   /****** Fetch Data ******/
   fetchData(id: Franchise['id']) {
     console.log(id);
   }
--- src/app/components/layouts/layout-form/layout-form.component.ts
+++ src/app/components/layouts/layout-form/layout-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-layout-form',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './layout-form.component.scss'
 })
 export class LayoutFormComponent {
+  @Input() title: string;
 
 }
